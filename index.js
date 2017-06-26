819ab16d426243223184c0cdead383a840733d11
zluo16 <zluo@alumni.berklee.edu>
2017-06-26 18:01:17 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log("printing");
+// }
+
+function add(a, b) {
+  return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+function inc(num) {
+  return num += 1;
+};
+
+function dec(num) {
+  return num -= 1;
+};
+
+function makeInt(string) {
+  return parseInt(string, 10);
+};
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+};
